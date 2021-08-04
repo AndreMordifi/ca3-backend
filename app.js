b6773d66766d385373ec259bc1bcc5b11dd5d261
AndreMordifi <andremordifii@gmail.com>
2021-08-04 22:27:11 +0800

    Attempted to fix CORS problem

--- app.js
+++ app.js
@@ -3,7 +3,9 @@ var express = require("express");
 var cors = require("cors");
 var app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
